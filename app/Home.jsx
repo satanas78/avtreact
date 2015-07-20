4f7b81f7365d973e15c03a1de2c683d48b1c7ebd
Vincent YAMAK <vincent.yamak@gmail.com>
2015-07-20 23:00:29 +0200

    data extraites de documentlist

--- app/Home.jsx
+++ app/Home.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import MenuActions from "./MenuActions";
 import SearchBar from "./SearchBar";
 import DocumentList from "./DocumentList";
+import {data} from "./data";
 
 export default class extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>Home</h1>
-				<MenuActions/>
-				<SearchBar/>
-				<DocumentList />
+				<MenuActions />
+				<SearchBar />
+				<DocumentList docs={data.docs} />
 			</div>
 		);
 	}
