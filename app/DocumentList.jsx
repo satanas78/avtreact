44a163a60b5f55c3aeba59d600b117fbdf10b534
Vincent YAMAK <vincent.yamak@gmail.com>
2015-07-21 08:00:28 +0200

    data extraites et injectées plus haut

--- app/DocumentList.jsx
+++ app/DocumentList.jsx
@@ -3,22 +3,10 @@ import { Link } from 'react-router';
 export default class extends React.Component {
 	constructor(props){
 		super(props);
-		this.state={docs:[
-			{
-				id: 1,
-				ref:'REF-001',
-				title:'Mon Titre', 
-				MonNombre:'123'
-			},{
-				id: 2,
-				ref:'REF-002',
-				title:'Mon Titre 2', 
-				MonNombre:'564'
-			}]};
 	}
 
 	render() { 
-		var docs = this.state.docs.map(doc => 				
+		var docs = this.props.docs.map(doc => 				
 			<tr key={doc.id}>
 				<td><Link to="document" params={{ id: doc.id }}>{doc.ref}</Link></td>
 				<td>{doc.title}</td>
--- app/Home.jsx
+++ app/Home.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import MenuActions from "./MenuActions";
 import SearchBar from "./SearchBar";
 import DocumentList from "./DocumentList";
+import {data} from "./data";
 
 export default class extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Home</h1>
+				<h1>Home2</h1>
 				<MenuActions/>
 				<SearchBar/>
-				<DocumentList />
+				<DocumentList docs={data.docs} />
 			</div>
 		);
 	}
